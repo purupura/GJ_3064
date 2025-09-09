49b3c3843d9efc7644a63f64d7138220443260a4
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-09-09 11:36:35 +0900

    aa

---
+++ DirectXGame/Player.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "kamataEngine.h"
+#include <assert.h>
+#include <list>
+using namespace KamataEngine;
+
+class Player
+{
+public:
+	~Player();
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+private:
+	
+};
+
