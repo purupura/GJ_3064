e40e852c3a35d6e7ef110fcf7388e3def9db8c2d
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-09-10 18:47:43 +0900

    a

--- DirectXGame/PlayerBullet.cpp
+++ DirectXGame/PlayerBullet.cpp
@@ -1,1 +1,48 @@
 #include "PlayerBullet.h"
+
+PlayerBullet::~PlayerBullet() {}
+
+void PlayerBullet::Initialize(Vector2 pos, Vector2 pos2) {
+	dxCommon_ = KamataEngine::DirectXCommon::GetInstance();
+	input_ = KamataEngine::Input::GetInstance();
+	textureHandle_ = KamataEngine::TextureManager::Load("Player/Player1Bullet.png");
+	sprite_ = Sprite::Create(textureHandle_, pos);
+	textureHandle2_ = KamataEngine::TextureManager::Load("Player/Player1Bullet_Another.png");
+	sprite2_ = Sprite::Create(textureHandle2_, pos2);
+}
+
+void PlayerBullet::Update() { Player1Bullet(); }
+
+void PlayerBullet::Draw() {
+	if (input_->PushKey(DIK_G)) {
+		sprite_->Draw();
+	    sprite2_->Draw();
+	}
+	
+}
+
+void PlayerBullet::Player1Bullet() {
+	Vector2 move = { 10.0, 1.0 };
+	// 一種類目の攻撃　仮に赤とする
+	Vector2 normalBulletPos = sprite_->GetPosition();
+	normalBulletPos.x += move.x;
+	sprite_->SetPosition(normalBulletPos);
+	// 二種類目の攻撃　仮に青とする
+	Vector2 AnotherBulletPos = sprite2_->GetPosition();
+	AnotherBulletPos.x += 5;
+	sprite2_->SetPosition(AnotherBulletPos);
+}
+
+bool PlayerBullet::IsOutOfScreen() {
+    // 画面サイズ（例: 1280x720、あなたのゲームに合わせて変更してください）
+    const float screenWidth  = 1280.0f;
+    const float screenHeight = 720.0f;
+
+    // 弾の座標
+	Vector2 pos = sprite_->GetPosition();
+
+    if (pos.x < 0 || pos.x > screenWidth || pos.y < 0 || pos.y > screenHeight) {
+        return true;
+    }
+    return false;
+}
