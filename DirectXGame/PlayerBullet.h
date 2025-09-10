db7ae5d5f3fca619fea0d45a115296d15d4c353b
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-09-10 18:46:04 +0900

    s

--- DirectXGame/PlayerBullet.h
+++ DirectXGame/PlayerBullet.h
@@ -1,5 +1,29 @@
 #pragma once
+#include "kamataEngine.h"
+#include <assert.h>
+
+using namespace KamataEngine;
 class PlayerBullet
 {
+public:
+	~PlayerBullet();
+
+	void Initialize(Vector2 pos, Vector2 pos2);
+
+	void Update();
+
+	void Draw();
+
+	void Player1Bullet();
+
+	bool IsOutOfScreen();
+private:
+	WorldTransform worldTransform_;
+	DirectXCommon* dxCommon_ = nullptr;
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	uint32_t textureHandle2_ = 0;
+	Sprite* sprite2_ = nullptr;
+	Input* input_ = nullptr;
 };
 
