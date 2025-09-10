558206c6efaa407a44bd3f158f831ba1f0be27c0
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-09-10 18:46:23 +0900

    a

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,5 +1,46 @@
 #pragma once
+#include "kamataEngine.h"
+#include <assert.h>
+#include <list>
+#include "WorldDimensionSwitching.h"
+#include "PlayerBullet.h"
+using namespace KamataEngine;
+
 class Player
 {
+public:
+	~Player();
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+	enum class Phase {  Move };
+
+	void Fire();
+
+	static const int kFireInterval = 60; // 発射間隔
+
+	void approach();
+private:
+	WorldTransform worldTransform_;
+	DirectXCommon* dxCommon_ = nullptr;
+	WorldDimensionSwitching* worldDimensionSwitching_ = nullptr;
+
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	uint32_t textureHandle2_ = 0;
+	Sprite* sprite2_ = nullptr;
+	Input* input_ = nullptr;
+	
+
+	Vector2 PlayerPosition_ = { 100.0f, 300.0f };
+
+	// 弾
+	std::list<PlayerBullet*> bullets_;
+	int32_t fireTimer_ = 0; // 発射タイマー
+
 };
 
